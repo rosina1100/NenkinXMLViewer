a95af20f51c50cb25e724550e7c51f1077ece586
rosina1100 <hiiro1100@gmail.com>
2026-03-18 15:24:05 +0900

    Initial Commit

---
+++ NenkinXMLViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NenkinXMLViewer
+//
+//  Created by 安藤大輝 on 2026/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NenkinXMLViewer/NenkinXMLViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NenkinXMLViewerApp.swift
+//  NenkinXMLViewer
+//
+//  Created by 安藤大輝 on 2026/03/18.
+//
+
+import SwiftUI
+
+@main
+struct NenkinXMLViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
